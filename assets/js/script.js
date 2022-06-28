20bf849735052866dffd791ce6c12e08a07218e3
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-06-28 17:30:49 -0500

    updated saveTextarea function to overwrite previous saves to the same timeBlock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
 // global variable used to iterate through array when needed (e.g., for setting time id for each div)
-var arrayIterator = 0;
-
+var hourArrayIterator = 0;
 // initializes an array that contains the ids for each div that creates the scheduler "blocks"
 var hourArray = ["eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen"];
+
+var saveArrayIterator = 0;
 
 // initializes the schedule upon page load
 var createScheduler = function() {
 // sets the current day to the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-// for loop to create schedule blocks
+// for loop to create schedule blocks; i=8; i<17 so that I can use the value of i to set the hour w/ moment js
 for (i=8; i<17; i++) {
     // dynamically creates a <div class='col-12 row' id='hour'>
-    var createBlockEl = $("<div class='col-12 row time-block' id='" + hourArray[arrayIterator] + "'>");
+    var createBlockEl = $("<div class='col-12 row time-block' id='" + hourArray[hourArrayIterator] + "'>");
 
     // dynamically creates a <span> element to house our time
     var spanEl = $("<span class='col-2 hour'>");
     spanEl.text(moment().set('hour',i).format("h a"));
     // dynamically creates the area we will be able to write to for scheduling
-    var textareaEl = $("<textarea class='col-8 description' id='" + hourArray[arrayIterator] + "'>");
+    var textareaEl = $("<textarea class='col-8 description' id='" + hourArray[hourArrayIterator] + "'>");
     // dynamically creates the button to save our entered data
-    var buttonEl = $("<button class='col-2 saveBtn' id='" + hourArray[arrayIterator] + "'>");
+    var buttonEl = $("<button class='col-2 saveBtn' id='" + hourArray[hourArrayIterator] + "'>");
     // creates the save icon for the button
     var iconEl = $("<i class='fa fa-save'>");
 
@@ -33,8 +34,8 @@ for (i=8; i<17; i++) {
     // appends the icon to the button
     $(buttonEl).append(iconEl);
 
-    // increase the global iterator we created to iterate 
-    arrayIterator++;
+    // increase the global iterator we created; This is used to iterate through the hourArray grabbing the correct id
+    hourArrayIterator++;
 };
 };
 
@@ -121,42 +122,81 @@ setInterval(function() {
 }, 10000);
 
 var saveTextarea = function(event) {
+    // grabs the id of the button which corresponds to the id of the schedule block it is in
     var timeBlock = $(event.target).attr('id');
+    // locates the text w/in the textarea which is stored under value
     var scheduleText = $("textarea#"+timeBlock)[0].value;
 
+    // neatly packs the text into an object that also tells the schedule block id
     scheduleObj = {
         id: timeBlock,
         text: scheduleText
     };
 
+    // grab any information from localStorage prior to sending our information
     scheduleArray = JSON.parse(localStorage.getItem("Schedule Info"));
 
+    // if my variable didn't exist in localStorage, create an empty array
     if (!scheduleArray) {
         scheduleArray = [];
-    }
+        scheduleArray.push(scheduleObj);
+    // if it already exist in localStorage... let's update it    
+    } else {
+        // initialize two variables to use for the while loop which will iterate like a for loop? I know... it's crazy
+        var i = -1;
+        var j = 0;
+        // while both conditions firstCondition and secondCondition are true... run my loop
+        // the firstCondition is using the return of .indexOf as logic. .indexOf returns -1 if the value timeBlock is not found within the array
+        // .indexOf returns 0 when it does find the correct value of timeBlock in the array - thus break the loop because we found what we want to write over
+        // the secondCondition is to iterate through the variable scheduleArray.length
+        // therefore, when it iterates through the final value of the array- it will break the loop since there was no match at all (thus stopping what would be a never-ending while loop)
+        while ((i < 0) && (j < scheduleArray.length)) {
+            // returns an array of the object parameters
+            var values = Object.values(scheduleArray[j]);
+            // returns -1 if the value of timeBlock is not within the object-made-array
+            i = values.indexOf(timeBlock);
+            // increment j to iterate through the scheduleArray.length until the end
+            j++;
+        };
+        // if we break out of the while loop because we matched timeBlock to an object parameter- overwrite that index's current value with what the saveBtn is trying to save currently
+        // this inhibits my scheduleArray from being infinitely long due to just pushing scheduleObj after scheduleObj that may or may not share the same timeBlock value
+        if (i === 0) {
+            // decrement j to get the true index of the array (this is necessary based on the while loop incrementing j AFTER we already found the index we want)
+            j--;
+            // overwrite the object here with the new object the saveBtn is trying to create
+            scheduleArray[j] = scheduleObj;
+        // else: if we broke out of the while loop due to exceeding scheduleArray.length
+        } else {
+            // simply push to the scheduleArray since an object with our particular timeBlock does not yet exist within scheduleArray
+            scheduleArray.push(scheduleObj);
+        }
+    };
 
-    scheduleArray.push(scheduleObj);
-
-    // code here to overwrite local storage based on id so it continuously updates and doesn't override
-
+    // sends all the objects within the scheduleArray to be stored in localStorage
     localStorage.setItem("Schedule Info", JSON.stringify(scheduleArray));
 };
 
 var loadSchedule = function() {
     scheduleArray = JSON.parse(localStorage.getItem("Schedule Info"));
     // need to code a loop that grabs the id and sets the text area value to the respectable boxes
-    console.log(scheduleArray);
-}
+};
 
 // calls createScheduler to create the schedule blocks
 createScheduler();
 
-// calls getId to populate schedule block background upon page load
-for (i = 0; i < hourArray.length; i++) {
-    getId($("textarea#"+hourArray[i]));
-};
+var initialize = function() {
+    for (i = 0; i < hourArray.length; i++) {
+        getId($("textarea#"+hourArray[i]));
+    };
+}
 
-$("#parent").on("click",'.saveBtn', saveTextarea);
+// calls getId to populate schedule block background upon page load
+initialize();
 
 // loads any previous tasks into the respectable schedule time blocks
 loadSchedule();
+
+// event delegation
+$("#parent").on("click",'.saveBtn', saveTextarea);
+
+// you get an error if they click on the icon. Fix that.
